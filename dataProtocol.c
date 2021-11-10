ed4e0d2325937bad321e728d0315da7c36ce39a9
Henrique <henriquecostasousa.hcs@gmail.com>
2021-11-10 18:42:20 +0000

    initializeFrame

--- dataProtocol.c
+++ dataProtocol.c
@@ -1,6 +1,24 @@
+#include "dataProtocol.h"
 
-void llopen(){
+void initializeFrame(struct frame *frame)
+{
+    char *newFrame = (char *)malloc(MAX_SIZE);
+    frame->stuffedFrame = newFrame;
+    frame->stuffedFrameSize = MAX_SIZE;
+}
 
+int llopen(int porta, int soup){
+    char* porta2 = malloc(sizeof(char) * (strlen(COM) + 1);
+    sprintf(porta2,%s%d,COM,porta);
+    int fd = setfd(porta);
+    switch(type) {
+        case SENDER:
+            if (senderSomething(fd) > 0) return fd; //emissor.c
+        case RECEiVER:
+            if (receiverSomething(fd) > 0) return fd; //recetor.c
+            
+    }
+    return -1;
 }
 
 void llwrite(){
@@ -11,6 +29,8 @@ void llread(){
 
 }
 
-void llclose(){
+int llclose(int fd){
     
+
+    return -1;
 }

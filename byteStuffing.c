98bf5292711ef3407ba5b58cf58f567e171fb40f
Henrique <henriquecostasousa.hcs@gmail.com>
2021-11-19 17:44:38 +0000

    Clean up

--- byteStuffing.c
+++ byteStuffing.c
@@ -4,74 +4,6 @@
 #include <string.h>
 #include "commandMessages.h"
 
-// int stuff(struct frame *frame, char tostuff[], int tostuffSize)
-// {
-//     if (frame->stuffedFrameSize == 0)
-//     {
-//         initializeFrame(frame); //Makes size of the stuffed frame to be MAX_SIZE
-//     }
-//     int sfCount = FIRST_DATA_INDEX; //stuffed count
-//     int nsfCount = 0;               //nonstuffed count
-//     int flagged = 0;
-//     char c, protectionByte = 0;
-
-//     for (int i = 0; i < tostuffSize; i++)
-//     {
-
-//         //Gets 20 more bytes of space in the frame
-//         if (sfCount == frame->stuffedFrameSize - 2) //-2 because that's the maximum amount of space that we will add in an iteration
-//         {
-//             allocSpace(frame, SPACE_ALLOC_SIZE);
-//         }
-
-//         c = tostuff[i];
-//         protectionByte ^= c;
-//         if (c == ESCAPE)
-//         {
-//             frame->frame[sfCount] = ESCAPE;
-//             frame->frame[sfCount + 1] = ESCAPE_THE_ESCAPE;
-//             sfCount += 2;
-//         }
-//         else if (c == F)
-//         {
-//             frame->frame[sfCount] = ESCAPE;
-//             frame->frame[sfCount + 1] = ESCAPE_THE_FLAG;
-//             sfCount += 2;
-//         }
-//         else
-//         {
-//             frame->frame[sfCount] = c;
-//             sfCount++;
-//         }
-//     }
-
-//     if (protectionByte == F) //Special and rare case
-//     {
-//         if (sfCount + 3 > frame->stuffedFrameSize) // the previous operation filled the buffer
-//         {
-//             allocSpace(frame, 3);
-//         }
-//         frame->frame[sfCount] = ESCAPE;
-//         sfCount++;
-//         frame->frame[sfCount] = ESCAPE_THE_FLAG;
-//         sfCount++;
-//         frame->frame[sfCount] = F;
-//         sfCount++;
-//     }
-//     else
-//     {
-//         if (sfCount + 2 > frame->stuffedFrameSize) // the previous operation filled the buffer
-//         {
-//             allocSpace(frame, 2);
-//         }
-//         frame->frame[sfCount] = protectionByte;
-//         sfCount++;
-//         frame->frame[sfCount] = F;
-//         sfCount++;
-//     }
-//     return 0;
-// }
-
 int stuff(struct frame *frame, char tostuff[], int tostuffSize)
 {
     // Tostuff will not overflow the buffer
@@ -189,23 +121,4 @@ int destuff(struct frame *frame, char *buffer)
     {
         return nsfCount - 1; //-1 because the protection byte that was also in the stuffed sequence doesn't belong to the data
     }
-}
-
-// void allocSpace(struct frame *frame, int space)
-// {
-//     char *newFrame = (char *)malloc(frame->stuffedFrameSize + space);
-//     memcpy((void *)newFrame, (void *)frame->frame, frame->stuffedFrameSize);
-//     free(frame->frame);
-//     frame->frame = newFrame;
-//     frame->stuffedFrameSize += space;
-// }
-
-int allocBufferSpace(char *buffer, int prevSpace, int extraSpace)
-{
-    int totalSpace = prevSpace + extraSpace;
-    char *newBuf = (char *)malloc(totalSpace);
-    memcpy(newBuf, buffer, prevSpace);
-    free(buffer);
-    buffer = newBuf;
-    return totalSpace;
 }
